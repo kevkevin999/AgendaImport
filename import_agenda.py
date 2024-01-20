3844dbfb21e3c8d3ecf3167f8e4544341c14ae46
kjx2 <94778542+kevkevin999@users.noreply.github.com>
2024-01-20 13:05:18 +0100

    outline for import agenda

---
+++ import_agenda.py
@@ -0,0 +1,11 @@
+# Library for reading data from an Excel Spreadsheet
+import xlrd;
+
+# Objective of this file: 
+# Reads data from the Excel spreadsheet (agenda.xls)
+# and populates our database with the data 
+
+# (From README)
+# 1. Open an Agenda excel file
+# 2. Design a SQLite Database table schema allowing to store agenda information
+# 3. Parse the content of the excel file and store the content in the table you designed
