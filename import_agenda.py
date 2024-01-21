a1a00f559988e15258f8be3518fe48eaf884a971
kjx2 <94778542+kevkevin999@users.noreply.github.com>
2024-01-21 16:31:26 +0100

    Set Up DB Tables in Import Agenda

--- import_agenda.py
+++ import_agenda.py
@@ -19,36 +19,36 @@ else:
     print('Minimum 1 argement expected')
     sys.exit()
 
-
 # 2. Design a SQLite Database table schema allowing to store agenda information
 # Table 1: All SESSIONs (each session will have an id, date, time start/end, title, location, description)
 # Table 2: All SUBSESSIONs (each subsession will have same columns as SESSIONs, but also which SESSION the subsession is a child of)
 # Table 3: Speakers (each speaker will be assosciated with a SESSION id - for sessions this is just the id, 
 # but for subsessions, this will be the session id of the parent - and an optional subsession id field and a session/subsession distinction). 
 
-# from db_table import db_table
-# users = db_table("session", {   "id": "integer PRIMARY KEY", 
-#                                 "date" : "date", 
-#                                 "time_start" : "text",
-#                                 "time_end" : "text",
-#                                 "title" : "text",
-#                                 "location" : "text",
-#                                 "description" : "text",  })
+from db_table import db_table
+sessions = db_table("sessions", { "id": "integer PRIMARY KEY", 
+                                "date": "date", 
+                                "time_start": "text", 
+                                "time_end": "text", 
+                                "title": "text", 
+                                "location": "text", 
+                                "description": "text"  })
 
-# users = db_table("subsession", {"id": "integer PRIMARY KEY",
-#                                 "parent_id" : "integer", 
-#                                 "date": "date", 
-#                                 "time_start": "text",
-#                                 "time_end": "text",
-#                                 "title" : "text",
-#                                 "location" : "text",
-#                                 "description" : "text",  })
+subsessions = db_table("subsessions", {"id": "integer PRIMARY KEY",
+                                "parent_id" : "integer", 
+                                "date": "date", 
+                                "time_start": "text",
+                                "time_end": "text",
+                                "title" : "text",
+                                "location" : "text",
+                                "description" : "text",  })
 
-# users = db_table("speaker", {   "name": "text", 
-#                                 "session" : "boolean", # true = session, false = subsession 
-#                                 "session_id" : "integer",
-#                                 "parent_id": "integer", # optional field 
-#                                 })
+speakers = db_table("speaker", {   "name": "text", 
+                                "session" : "boolean", # true = session, false = subsession 
+                                "session_id" : "integer",
+                                "parent_id": "integer", # optional field 
+                                })
+
 
 # 3. Parse the content of the excel file and store the content in the table you designed
 # While parsing the excel file, keep a running count of the number of sessions counted (id)
@@ -57,9 +57,14 @@ else:
 # Open the 'Agenda' sheet
 sh = agenda.sheet_by_index(0)
 for row in range (14, sh.nrows):
+    # if (sh.cell(row, 3) == 'Session'):
     values = []
     for col in range (sh.ncols): 
         values.append(sh.cell(row, col).value)
     print (','.join(values))
 print
+
+sessions.close()
+subsessions.close()
+speakers.close()
 
