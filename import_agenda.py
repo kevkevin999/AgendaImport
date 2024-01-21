ee7ede8d7f26d9227c5212e3215e928205809412
kjx2 <94778542+kevkevin999@users.noreply.github.com>
2024-01-21 16:05:25 +0100

    Added Excel File Parsing

--- import_agenda.py
+++ import_agenda.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python
+
+# For reading command line argments
+import sys
 # Library for reading data from an Excel Spreadsheet
 import xlrd
+
 
 # Objective of this file: 
 # Reads data from the Excel spreadsheet (agenda.xls)
@@ -7,7 +12,13 @@ import xlrd
 
 # (From README)
 # 1. Open an Agenda excel file
-agenda = xlrd.open_workbook("agenda.xls")
+if (len(sys.argv) > 1):
+    agenda = xlrd.open_workbook(sys.argv[1])
+    print('Worksheet opened!')
+else:
+    print('Minimum 1 argement expected')
+    sys.exit()
+
 
 # 2. Design a SQLite Database table schema allowing to store agenda information
 # Table 1: All SESSIONs (each session will have an id, date, time start/end, title, location, description)
@@ -15,6 +26,40 @@ agenda = xlrd.open_workbook("agenda.xls")
 # Table 3: Speakers (each speaker will be assosciated with a SESSION id - for sessions this is just the id, 
 # but for subsessions, this will be the session id of the parent - and an optional subsession id field and a session/subsession distinction). 
 
+# from db_table import db_table
+# users = db_table("session", {   "id": "integer PRIMARY KEY", 
+#                                 "date" : "date", 
+#                                 "time_start" : "text",
+#                                 "time_end" : "text",
+#                                 "title" : "text",
+#                                 "location" : "text",
+#                                 "description" : "text",  })
+
+# users = db_table("subsession", {"id": "integer PRIMARY KEY",
+#                                 "parent_id" : "integer", 
+#                                 "date": "date", 
+#                                 "time_start": "text",
+#                                 "time_end": "text",
+#                                 "title" : "text",
+#                                 "location" : "text",
+#                                 "description" : "text",  })
+
+# users = db_table("speaker", {   "name": "text", 
+#                                 "session" : "boolean", # true = session, false = subsession 
+#                                 "session_id" : "integer",
+#                                 "parent_id": "integer", # optional field 
+#                                 })
+
 # 3. Parse the content of the excel file and store the content in the table you designed
 # While parsing the excel file, keep a running count of the number of sessions counted (id)
 # Each subsession's parent id is equal to the above number
+
+# Open the 'Agenda' sheet
+sh = agenda.sheet_by_index(0)
+for row in range (14, sh.nrows):
+    values = []
+    for col in range (sh.ncols): 
+        values.append(sh.cell(row, col).value)
+    print (','.join(values))
+print
+
