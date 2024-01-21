a5b2f4ef138ae96661ee3921bc25d5f78fcad942
kjx2 <94778542+kevkevin999@users.noreply.github.com>
2024-01-21 17:15:03 +0100

    Import agenda populate database

--- import_agenda.py
+++ import_agenda.py
@@ -26,28 +26,33 @@ else:
 # but for subsessions, this will be the session id of the parent - and an optional subsession id field and a session/subsession distinction). 
 
 from db_table import db_table
-sessions = db_table("sessions", { "id": "integer PRIMARY KEY", 
-                                "date": "date", 
-                                "time_start": "text", 
-                                "time_end": "text", 
-                                "title": "text", 
-                                "location": "text", 
-                                "description": "text"  })
+sessions = db_table("sessions", { 
+    "id": "integer PRIMARY KEY", 
+    "date": "date", 
+    "time_start": "text", 
+    "time_end": "text", 
+    "title": "text", 
+    "location": "text", 
+    "description": "text"  
+})
 
-subsessions = db_table("subsessions", {"id": "integer PRIMARY KEY",
-                                "parent_id" : "integer", 
-                                "date": "date", 
-                                "time_start": "text",
-                                "time_end": "text",
-                                "title" : "text",
-                                "location" : "text",
-                                "description" : "text",  })
+subsessions = db_table("subsessions", {
+    "id": "integer PRIMARY KEY",
+    "parent_id" : "integer", 
+    "date": "date", 
+    "time_start": "text",
+    "time_end": "text",
+    "title" : "text",
+    "location" : "text",
+    "description" : "text",  
+})
 
-speakers = db_table("speaker", {   "name": "text", 
-                                "session" : "boolean", # true = session, false = subsession 
-                                "session_id" : "integer",
-                                "parent_id": "integer", # optional field 
-                                })
+speakers = db_table("speaker", {   
+    "name": "text", 
+    "session" : "boolean", # true = session, false = subsession 
+    "session_id" : "integer",
+    "parent_id": "integer", # optional field 
+})
 
 
 # 3. Parse the content of the excel file and store the content in the table you designed
@@ -56,13 +61,53 @@ speakers = db_table("speaker", {   "name": "text",
 
 # Open the 'Agenda' sheet
 sh = agenda.sheet_by_index(0)
-for row in range (14, sh.nrows):
-    # if (sh.cell(row, 3) == 'Session'):
-    values = []
-    for col in range (sh.ncols): 
-        values.append(sh.cell(row, col).value)
-    print (','.join(values))
-print
+session_id = -1 # Assume the first row is NOT a subsession
+subsession_id = -1
+currently_session = False
+
+# For each row starting from row 16
+for row in range (15, sh.nrows):
+    # Add data to the correct table
+    # TODO need to account for commas in strings (causing sytax errors for db)
+    if (sh.cell(row, 3).value.strip() == 'Session'):
+        session_id = row
+        currently_session = True
+        sessions.insert({   
+            "id" : str(row), 
+            "date" : sh.cell(row, 0).value,
+            "time_start" : sh.cell(row, 1).value,
+            "time_end" : sh.cell(row, 2).value,
+            "title" : sh.cell(row, 4).value,
+            "location" : sh.cell(row, 5).value,
+            "description" : "\"" + sh.cell(row, 6).value + "\""
+        })
+    elif (sh.cell(row, 3).value.strip() == 'Sub'):
+        subsession_id = row
+        currently_session = False
+        subsessions.insert({   
+            "id" : str(subsession_id),
+            "parent_id" : str(session_id), 
+            "date" : sh.cell(row, 0).value,
+            "time_start" : sh.cell(row, 1).value,
+            "time_end" : sh.cell(row, 2).value,
+            "title" : sh.cell(row, 4).value,
+            "location" : sh.cell(row, 5).value,
+            "description" : sh.cell(row, 6).value 
+        })
+    else:
+        print('Unable to determine whether the event is a SESSION or a SUBSESSION')
+    
+    # Add data to the speakers table (if the speaker cell is not empty)
+        # TODO looks like empty speakers are still being added...
+    if (sh.cell(row, 7)):
+        event_speakers = sh.cell(row, 7).value.split(';')
+        for speaker in event_speakers:
+            speakers.insert({
+                "name" : "{speaker.strip()}",
+                "session" : currently_session,
+                "session_id": session_id if currently_session else subsession_id ,
+                "parent_id": "" if currently_session else session_id,
+            })
 
 sessions.close()
 subsessions.close()
