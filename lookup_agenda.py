d94de662abfebace60e5392155cc3307ea430dab
kjx2 <94778542+kevkevin999@users.noreply.github.com>
2024-01-23 19:14:53 +0100

    Added session/subsession to Output

--- lookup_agenda.py
+++ lookup_agenda.py
@@ -11,7 +11,7 @@ column_types = {'date', 'time_start', 'time_end', 'title', 'location', 'descript
 
 if (len(sys.argv) == 3):
     column = sys.argv[1].lower()
-    input = sys.argv[2]
+    input = sys.argv[2].replace("'", "''")
     print("%s : %s" % (column, input))
     if (column not in column_types):
         print('Argument 1 does not match an existing column')
@@ -63,38 +63,45 @@ values = []
 if (column == 'speaker'):
     speakers_output = speakers.select(where={"name" : input})
     for row in speakers_output:
-        # print(row)
         if (row['session'] == 'True'):
-            # look in session table
-            print('session')
-            values.append(sessions.select(where={"id" : row["session_id"]}))
+            session_results = sessions.select(where={"id" : row["session_id"]})
+            for result in session_results:
+                result['session'] = 'Session'
+                values.append(result)
         else:
-            print('subsession')
-            values.append(subsessions.select(where={"id" : row["session_id"]}))
+            subsession_results = subsessions.select(where={"id" : row["session_id"]})
+            for result in subsession_results:
+                result['session'] = 'Subsession'
+                values.append(result)
 else:
-    sessions_output = sessions.select(where={column : input})
     session_id_set = []
+    sessions_output = sessions.select(where={column : input})
     # if a subsession has a parent_id that matches an id from the above query, also return that row
     for row in sessions_output:
+        row['session'] = 'Session'
         values.append(row)
         session_id_set.append(row['id'])
 
     subsession_output = subsessions.select(where={column : input})
     for row in subsession_output:
+        row['session'] = 'Subsession'
         values.append(row)
 
     for session_id in session_id_set:
         results = subsessions.select(where={"parent_id" : session_id}) # TODO only add to values if result != []
         if results != []:
             for result in results:
+                result['session'] = 'Subsession'
                 values.append(result)
 
 
 # 3. Print the result onto the screen
 if (values.count == 0):
     print('No Results Found')
+# elif (column != "speaker"):
 for value in values:
-    print("%s\t%s\t%s\t%s\t%s\t%s" % (value['date'], value['time_start'], value['time_end'], value['title'], value['location'], value['description']))
+    print("%s\t%s\t%s\t%s\t%s\t%s\t%s" % (value['date'], value['time_start'], value['time_end'], value['title'], value['location'], value['description'], value['session']))
+
 
 sessions.close()
 subsessions.close()
